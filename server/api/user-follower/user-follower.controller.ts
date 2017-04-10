cef53d1115fdb7febe600fcf7f2e829734ca1a8a
Andrew King <luv2bking@gmail.com>
2017-04-10 11:14:05 -0600

    added import statement

--- server/api/user-follower/user-follower.controller.ts
+++ server/api/user-follower/user-follower.controller.ts
@@ -1,5 +1,6 @@
 import * as _ from 'lodash';
 import { UserFollower, IUserFollower } from './user-follower.model';
+import {UserFollowerOperations} from './user-follower.operations'
 
 export class UserFollowerCtrl {
  
