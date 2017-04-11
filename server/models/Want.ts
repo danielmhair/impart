cfea115a7781a1871125db4c2b18bace1d612366
parsonsjo1 <jopa.d.parsons@gmail.com>
2017-04-11 12:09:20 -0600

    merged conflicting chanages in user controller.

--- server/models/Want.ts
+++ server/models/Want.ts
@@ -1,4 +1,9 @@
 export class Want {
   public categories: string[];
   public endpoint: string;
+
+  constructor(categories: string[], endpoint: string) {
+    this.categories = categories;
+    this.endpoint = endpoint;
+  }
 }
